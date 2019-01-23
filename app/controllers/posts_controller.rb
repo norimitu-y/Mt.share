2bb1e188691d0adf1310b5f4dd0146ee129c8731
安井 教光 <norimitu@iMac.local>
2019-01-23 23:35:08 +0900

    ransack追加まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_post, only:[:show, :edit, :update]
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
 
 
   def index
-    @posts = Post.all
+    @q = Post.search(params[:q])
+    @posts = @q.result
   end
 
   def new
@@ -20,6 +21,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    render layout: false
   end
 
   def edit
@@ -31,6 +33,11 @@ class PostsController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました"
   end
 
 
